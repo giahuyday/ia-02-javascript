7c0b9770e1ba6302b54e956376eb00e2aa415b1c
giahuyday <huyhoangnguyen301202@gmail.com>
2023-10-29 22:33:36 +0700

    [fix] math onClick position from label to input field

---
+++ index.js
@@ -0,0 +1,43 @@
+let operator = '';
+
+function setMath(name) {
+    operator = name.value;
+}
+
+function calculate() {
+    const resultElement = document.getElementById('result');
+    const number1Field = document.getElementById('number1');
+    const number2Field = document.getElementById('number2');
+
+    const number1 = parseFloat(number1Field.value);
+    const number2 = parseFloat(number2Field.value);
+
+    if (isNaN(number1) || isNaN(number2)) {
+        window.alert("Vui lòng nhập số thực hơp lệ và đủ 2 số!")
+        resultElement.value = "Vui lòng nhập số thực hơp lệ và đủ số!"
+        return;
+    }
+    console.log(operator)
+
+    switch (operator) {
+        case 'addition':
+            resultElement.value = number1 + number2;
+            break;
+        case 'substraction':
+            resultElement.value = number1 - number2;
+            break;
+        case 'multiple':
+            resultElement.value = number1 * number2;
+            break;
+        case 'division':
+            if (number2 !== 0) {
+                resultElement.value = number1 / number2;
+            } else {
+                resultElement.value = 'Không thể chia cho 0.';
+            }
+            break;
+        default:
+            window.alert("Vui lòng chọn phép toán!")
+            resultElement.value = 'Vui lòng chọn phép toán.';
+    }
+}
